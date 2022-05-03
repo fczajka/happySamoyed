f205c3884851bd2c80773c16491a21c9ab8b0438
FranciszekCzajka <franekczajkaa@gmail.com>
2022-05-04 00:01:22 +0200

    Initial commit + setup

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
