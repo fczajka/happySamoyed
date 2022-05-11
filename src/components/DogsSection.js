ca32fbb924da9de20098ca4bcfd51378d4b47e39
FranciszekCzajka <franekczajkaa@gmail.com>
2022-05-11 22:26:36 +0200

    Change font type in 'dogs' section and 'why us' section

--- src/components/DogsSection.js
+++ src/components/DogsSection.js
@@ -5,7 +5,7 @@ import samoyed2Photo from "../images/William.webp";
 const DogsSection = () => {
     return (
         <div className="w-full flex justify-center items-center flex-col py-10 text-babyBlue-1500 xsm:px-10 md:px-20 2xl:px-28">
-            <h2 className="font-radio-canada text-4xl my-16 xsm:text-5xl">
+            <h2 className="font-titan-one text-4xl my-16 xsm:text-5xl">
                 Moje psy:
             </h2>
             <DogInfo
--- src/components/WhyUs.js
+++ src/components/WhyUs.js
@@ -8,7 +8,7 @@ import walkingPic from "../images/samoyedWalking.webp";
 const WhyUs = () => {
     return (
         <div className="w-full flex justify-center items-center flex-col py-10 md:px-20 2xl:px-28">
-            <h2 className="font-radio-canada text-4xl mt-16 text-babyBlue-1500 xsm:text-5xl">
+            <h2 className="font-titan-one text-4xl mt-16 text-babyBlue-1500 xsm:text-5xl">
                 Dlaczego my:
             </h2>
             <div className="flex w-full items-center flex-col pt-8 xl:flex-row xl:justify-between 2xl:justify-evenly">
